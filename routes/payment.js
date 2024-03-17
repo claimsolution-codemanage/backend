1cfcb12793044f845226a08babb8da796fb64850
Deepak <maskman.deepak911@gmail.com>
2024-03-17 22:24:57 +0530

    update

--- routes/payment.js
+++ routes/payment.js
@@ -1,8 +1,7 @@
 import express from 'express';
 const router = express.Router();
-import { paymentWebHook,paymentCheckoutPage,paymentConfirmation } from '../controller/payment.js';
+import { paymentWebHook,paymentCheckoutPage} from '../controller/payment.js';
 
 router.post("/paymentWebHook",paymentWebHook)
 router.get("/paymentCheckoutPage",paymentCheckoutPage)
-router.get("/confirmation",paymentConfirmation)
 export default router
